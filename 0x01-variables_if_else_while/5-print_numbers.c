6b3a46ed501d2d6db62be6bdbf9912cc5c5f89ab
mo7amedsalah3li <mo7ammedsalaha39@gmail.com>
2023-03-17 12:08:55 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet without e,q.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int n;
+
+		n = 0;
+		while
+			(n <= 9)
+			{
+				putchar(n);
+				n++;
+			}
+		putchar('\n');
+		return (0);
+}
