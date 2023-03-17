94231f82c21b5218f43f25bbdc59c58be19c4a9f
mo7amedsalah3li <mo7ammedsalaha39@gmail.com>
2023-03-17 11:31:48 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i != 5 && i != 17)
+			putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
