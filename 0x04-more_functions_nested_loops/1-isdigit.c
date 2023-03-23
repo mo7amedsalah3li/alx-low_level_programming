1613397c14fc74dbeb243eec8ab012019591a987
mo7amedsalah3li <mo7ammedsalaha39@gmail.com>
2023-03-23 05:19:47 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *@c: Variable that stores the letters
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
