ef6fea230f0720ea43d4548296dcab60101a5038
mo7amedsalah3li <mo7ammedsalaha39@gmail.com>
2023-03-23 04:45:56 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * Return: Always 0.
+ */
+int int _isupper(int c);
+{
+	for ( c = 'A'; c <= 'Z'; c++)
+		return (1);
+	else
+		return (0);
+}
